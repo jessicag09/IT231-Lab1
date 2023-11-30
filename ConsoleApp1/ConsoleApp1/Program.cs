79dcc4dec9b45476dbf811cd3f6e5df34a377d6d
jessicag09 <jessica.gravel555@gmail.com>
2023-11-30 11:03:18 -0500

    commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World! - mult");
+Console.WriteLine("Enter a number:");
+int num1 = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter another number:");
+int num2 = Int32.Parse(Console.ReadLine());
+
